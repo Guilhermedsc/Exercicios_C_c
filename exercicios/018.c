ccc8a84812d6817dbd2461396b37d66df354c623
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:23:34 -0300

    Update 018.c

--- exercicios/018.c
+++ exercicios/018.c
@@ -1,27 +1,22 @@
 /*
-Faça um programa que peça o tamanho de um arquivo para download (em MB) e a
-velocidade de um link de Internet (em Mbps), calcule e informe o tempo
-aproximado de download do arquivo usando este link (em minutos).
+Faça um programa em Linguagem C que leia uma matriz 6 x 6, conte e escreva quantos valores maiores que 10 ela possui.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include<stdio.h>
+#include<locale.h> /* Arquivo de cabeçalho que nos permite utilizar a função setlocale que faz a função printf() imprimir na tela caracteres da língua Portuguesa como por exemplo ç , á, ã */
 int main()
 {
-    float tamanhoDoArquivo,
-        velocidadeDaInternet,
-        tamanhoDoArquivoEmMegaBits,
-        tempoDeDownload;
-    printf("Digite o tamanho do arquivo em MB (Mega Bytes): ");
-    scanf("%f", &tamanhoDoArquivo);
-    printf("Digite a velocidade da internet em Mbps (Mega bits por segundo): ");
-    scanf("%f", &velocidadeDaInternet);
-    // Eu criei a variável abaixo apenas para deixar claro que 1MB são 8Mb.
-    // Ela não é totalmente necessária
-    tamanhoDoArquivoEmMegaBits = tamanhoDoArquivo * 8;
-    tempoDeDownload = tamanhoDoArquivoEmMegaBits / velocidadeDaInternet;
-    printf("O tempo aproximado para o download do arquivo é de %.0f segundos",
-           tempoDeDownload);
-    return 0;
+    int linha, coluna, matriz[6][6], contador=0;
+    setlocale(LC_ALL,"PORTUGUESE");
+    for( linha=0; linha<6; linha++ )
+    {
+        for( coluna=0; coluna<6; coluna++ )
+        {
+           printf("Digite o valor de [%d][%d] = ", linha+1, coluna+1);
+           scanf("%d", &matriz[linha][coluna]);
+           if( matriz[linha][coluna] > 10 )
+               contador++;
+        }
+    }
+    printf("\n\n números maiores que Dez = %d \n\n", contador);
+  return 0;    
 }
