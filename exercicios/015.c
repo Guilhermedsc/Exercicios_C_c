fda39f7f82e11c6082540b346974a126cc269351
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:14:56 -0300

    Update 015.c

--- exercicios/015.c
+++ exercicios/015.c
@@ -1,54 +1,40 @@
 /*
-Faça um Programa que pergunte quanto você ganha por hora e o número de horas
-trabalhadas no mês.
-
-Calcule e mostre o total do seu salário no referido mês,
-sabendo-se que são descontados 11% para o Imposto de Renda,
-8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
-    salário bruto.
-    quanto pagou ao INSS.
-    quanto pagou ao sindicato.
-    o salário líquido.
-calcule os descontos e o salário líquido, conforme a tabela abaixo:
-
-    + Salário Bruto : R$
-    - IR (11%) : R$
-    - INSS (8%) : R$
-    - Sindicato ( 5%) : R$
-    = Salário Liquido : R$
-
-Obs.: Salário Bruto - Descontos = Salário Líquido.
+Fazer um algoritmo que:
+Leia um vetor contendo 10 números, que correspondem a matrículas de alunos. Ler 3 matrículas e imprima uma mensagem informando se eles estão ou não presentes no vetor.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float salarioHora,
-        horasTrabalhadas,
-        IR,
-        INSS,
-        sindicato,
-        salarioLiquido,
-        salarioBruto;
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
 
-    printf("Digite quanto voce recebe por hora: ");
-    scanf("%f", &salarioHora);
-    printf("Digite quantas horas voce trabalhou esse mes: ");
-    getchar();
-    scanf("%f", &horasTrabalhadas);
+int main(void){
 
-    salarioBruto = salarioHora * horasTrabalhadas;
-    IR = salarioBruto * (11.0 / 100);
-    INSS = salarioBruto * (8.0 / 100);
-    sindicato = salarioBruto * (5.0 / 100);
-    salarioLiquido = salarioBruto - IR - INSS - sindicato;
+	int vetor[10],contagem, verificanumero, contagemverifica = 0, contemcerto = 0;
 
-    printf("+ Salário Bruto : R$%.2f\n", salarioBruto);
-    printf("- IR (11%) : R$%.2f\n", IR);
-    printf("- INSS (8%) : R$%.2f\n", INSS);
-    printf("- Sindicato ( 5%) : R$%.2f\n", sindicato);
-    printf("= Salário Liquido : R$%.2f", salarioLiquido);
-    return 0;
+	for(contagem = 0; contagem < 10; contagem++){
+
+		scanf("%d",&vetor[contagem]);
+
+	}
+
+	while (contagemverifica < 3){
+
+	scanf("%d",&verificanumero);
+
+		for(contagem = 0; contagem < 10; contagem++){
+
+			if (verificanumero == vetor[contagem]){
+				printf("A matricula %d esta presente no sistema\n",verificanumero);
+				contemcerto++;
+			}
+
+		}
+			if(contemcerto == 0){
+				printf("A matricula %d nao esta presente no sistema\n",verificanumero);
+			}
+
+	contagemverifica++;
+	contemcerto = 0;
+	}
+
+	return 0;
 }
