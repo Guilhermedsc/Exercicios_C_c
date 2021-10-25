ff75e2d3fccfde9af9a1c103eb1355a18fb95269
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:03:55 -0300

    Update 008.c

--- exercicios/008.c
+++ exercicios/008.c
@@ -8,19 +8,49 @@ para calcular a conta é:
 O programa deverá ler a conta do cliente, consumo de água por metros cúbicos e o tipo de consumidor ( residencial, comercial e industrial ). Como resultado, imprima 
 a conta do cliente e o valor real a ser pago pelo mesmo.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
 int main()
 {
-    float salarioHr, salarioMe, horasTrabalhadas;
-    printf("Digite quanto voce ganha por hora: ");
-    scanf("%f", &salarioHr);
-    printf("Digite quantas horas voce trabalhou esse mes: ");
-    getchar();
-    scanf("%f", &horasTrabalhadas);
-    salarioMe = salarioHr * horasTrabalhadas;
-    printf("Seu salario total do mes eh de R$%.2f", salarioMe);
-    return 0;
+int contacliente, consumoagua;
+float formula;
+char tipoconsumidor[15];
+
+scanf("%d",&contacliente);
+scanf("%d",&consumoagua);
+scanf("%s",tipoconsumidor);
+
+if (strcmp(tipoconsumidor,"RESIDENCIAL") == 0){
+formula = 5.00 + (0.05 * consumoagua);
+printf("CONTA = %d\n",contacliente);
+printf("VALOR DA CONTA = %.2f\n",formula);
+}
+
+if (strcmp(tipoconsumidor,"COMERCIAL") == 0 && consumoagua <= 80 ){
+formula = 500.00;
+printf("CONTA = %d\n",contacliente);
+printf("VALOR DA CONTA = %.2f\n",formula);
+}
+
+else if (strcmp(tipoconsumidor,"COMERCIAL") == 0 && consumoagua > 80 ){
+formula = 500.00 + ((consumoagua - 80) * 0.25);
+printf("CONTA = %d\n",contacliente);
+printf("VALOR DA CONTA = %.2f\n",formula);
+}
+
+if (strcmp(tipoconsumidor,"INDUSTRIAL") == 0 && consumoagua <= 100){
+formula = 800.00;
+printf("CONTA = %d\n",contacliente);
+printf("VALOR DA CONTA = %.2f",formula);
+}
+else if (strcmp(tipoconsumidor,"INDUSTRIAL") == 0 && consumoagua > 100){
+formula = 800.00 + ((consumoagua - 100 )* 0.04);
+printf("CONTA = %d\n",contacliente);
+printf("VALOR DA CONTA = %.2f\n",formula);
+}
+
+return 0;	
+
 }
