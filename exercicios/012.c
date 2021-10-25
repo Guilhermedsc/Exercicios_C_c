e450bfa314a40b70bd652a06533d981c5c1969ad
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:12:05 -0300

    Update 012.c

--- exercicios/012.c
+++ exercicios/012.c
@@ -1,18 +1,36 @@
 /*
-Tendo como dados de entrada a altura de uma pessoa,
-construa um algoritmo que calcule seu peso ideal,
-usando a seguinte fórmula: (72.7*altura) - 58
+Fazer um algoritmo que:
+Leia números de matrículas de alunos e armazene-os em um vetor até o vetor ser preenchido por 10 matrículas. Esses números são distintos, ou seja, o 
+vetor não armazenará valores repetidos.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float altura, pesoIdeal;
-    printf("Digite sua altura: ");
-    scanf("%f", &altura);
-    pesoIdeal = (72.7 * altura) - 58;
-    printf("Seu peso ideal eh: %.2fkg", pesoIdeal);
-    return 0;
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+int vetor[10], numero, cont, posicao = 0 ;
+
+while (posicao < 10){
+
+scanf("%d",&numero);
+
+if (posicao == 0){
+vetor[posicao] = numero;
+printf("%d\n",vetor[posicao]);
+posicao++;
+}
+
+else{
+
+for(cont = 0; (cont < posicao)&&(vetor[cont]!= numero); cont++);
+
+if (cont >= posicao){
+vetor[posicao] = numero;
+printf("%d\n",vetor[posicao]);
+posicao++;
+}
+}
+}
+return 0;
 }
