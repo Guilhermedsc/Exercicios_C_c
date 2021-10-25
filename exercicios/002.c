11e4c7fcaa203ca1d980683fd868733f433c5c91
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 14:53:13 -0300

    Update 002.c

--- exercicios/002.c
+++ exercicios/002.c
@@ -1,16 +1,42 @@
 /*
-Faça um Programa que peça um número e então mostre a mensagem O número
-informado foi [número]. 
+Dados quatro números distintos, desenvolver um algoritmo que determine e imprima a soma dos três menores.
 */
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<stdio.h>
+#include<math.h>
 
-int main(){
-    int n=0;
-    std::cout << "Digite um numero\n";
-    std::cin >> n;
-    std::cout << "O numero digitado foi: " << n;
+int main()
+{
+int numero1, numero2, numero3, numero4, soma;
 
-    return 0;
+scanf("%d",&numero1);
+scanf("%d",&numero2);
+scanf("%d",&numero3);
+scanf("%d",&numero4);
+
+if(numero1 > numero2 && numero1 > numero3 && numero1 > numero4)
+{
+soma = (numero2 + numero3 + numero4);
+printf("%d\n",soma);
+}
+
+else if(numero2 > numero1 && numero2 > numero3 && numero2 > numero4)
+{
+soma = numero1 + numero3 + numero4;
+printf("%d\n",soma);
+}
+
+else if(numero3 > numero1 && numero3 > numero2 && numero3 > numero4)
+{
+soma = numero1 + numero2 + numero4;
+printf("%d\n",soma);
+}
+
+else if(numero4 > numero1 && numero4 > numero2 && numero4 > numero3)
+{
+soma = numero1 + numero2 + numero3;
+printf("%d\n",soma);
+}
+
+return 0;
+
 }
