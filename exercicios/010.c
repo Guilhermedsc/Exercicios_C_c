39b74f420b75e890907a66e6a328c370bcf585d4
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:07:22 -0300

    Update 010.c

--- exercicios/010.c
+++ exercicios/010.c
@@ -1,17 +1,36 @@
 /*
-Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre
-em graus Farenheit.
+Um certo aço é classificado de acordo com o resultado de três testes abaixo, que devem determinar se o mesmo satisfaz as especificações:
+1. Conteúdo de Carbono abaixo de 7;
+2. Dureza Rockwell maior do que 50;
+3. Resistência á tração maior do que 80.000 psi;
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
 int main()
 {
-    float grausC, grausF;
-    printf("Digite a temperatura em graus Celsius: ");
-    scanf("%f", &grausC);
-    grausF = (grausC * 9 / 5) + 32;
-    printf("%.2f graus Celsius correspondem a %.2f graus Farenheit", grausC, grausF);
-    return 0;
+int conteudo, dureza, resistencia;
+
+scanf("%d",&conteudo);
+scanf("%d",&dureza);
+scanf("%d",&resistencia);    
+
+if (conteudo < 7 && dureza > 50 && resistencia > 80000){
+printf("ACO DE GRAU = 10\n");
+}
+
+else if (conteudo < 7 && dureza > 50 && resistencia <= 80000) {
+printf("ACO DE GRAU = 9\n");
+}
+
+else if (conteudo < 7 && dureza <= 50 && resistencia <= 80000){
+printf("ACO DE GRAU = 8\n");
+}
+
+else if (conteudo >= 7 && dureza <= 50 && resistencia <= 80000){
+printf("ACO DE GRAU = 7\n");
+}	
+
+return 0;
 }
