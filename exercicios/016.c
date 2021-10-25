6650de1b2e98b2aabb5638f94e9404daec63b588
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:21:23 -0300

    Update 016.c

--- exercicios/016.c
+++ exercicios/016.c
@@ -1,33 +1,31 @@
 /*
-Faça um programa para uma loja de tintas.
-O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
-
-Considere que a cobertura da tinta é de 1 litro para cada 3 metros
-quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
-
-Informe ao usuário a quantidades de latas de tinta
-a serem compradas e o preço total.
+Crie um programa que preencha todos os valores de uma matriz 2x2, e em seguida exiba todos os valores e a soma de todos os valores.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include<stdio.h>
+#include<locale.h> /* Arquivo de cabeçalho que nos permite utilizar a função setlocale que faz a função printf() imprimir na tela caracteres da língua Portuguesa como por exemplo ç , á, ã */
 int main()
 {
-    float areaDaParede,
-        rendimentoDoLitro = 3.0,
-        litrosPorLata = 18.0,
-        precoDaLata = 80.0,
-        precoTotal;
-    int latasAComprar;
+float matriz[2][2], soma=0;
+int i,j;
+setlocale(LC_ALL,"PORTUGUESE");
+printf("\t \n Digite os valores da matriz : \n");
+for(i=0;i<2;i++) /*como a gente precisa exibir uma matriz de duas linhas e duas colunas e a contagem começa em zero, temos que colocar i<2 pra termos as duas linhas, ou seja, i[0] e i[1] e o mesmo acontece com as colunas j[0] e j[1] pra totalizar 4 elementos (2*2=4)*/
+for(j=0;j<2;j++)
+{
+ scanf("%f",&matriz[i][j]);
+}
 
-    printf("Digite a area da parede em metros quadrados: ");
-    scanf("%f", &areaDaParede);
-
-    latasAComprar = ceil(areaDaParede / (rendimentoDoLitro * litrosPorLata));
-    //ceil é uma função da biblioteca matemática
-    precoTotal = latasAComprar * precoDaLata;
-    printf("Voce deve comprar %i lata(s), custando R$%.2f",
-           latasAComprar, precoTotal);
-    return 0;
+for(i=0;i<2;i++)
+for(j=0;j<2;j++)
+{
+  soma=soma+matriz[i][j];
+}
+printf(" Os valores são: \n");
+for(i=0;i<2;i++)
+for(j=0;j<2;j++)
+  {
+     printf("%.1f\n",matriz[i][j]);
+  }
+ printf("A soma é = %.1f\n",soma);
+ return 0; 
 }
