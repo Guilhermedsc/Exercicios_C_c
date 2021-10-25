ee215de8292cf2fc52754f52613eabd380470778
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 14:50:11 -0300

    Update 001.c

--- exercicios/001.c
+++ exercicios/001.c
@@ -1,10 +1,20 @@
 /*
-Faça um Programa que mostre a mensagem "Alo mundo" na tela
+Desenvolver um algoritmo que leia um número inteiro e verifique se o número é divisível por 5 e por 3 ao mesmo tempo.
 */
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<stdio.h>
+#include<math.h>
 
-int main(){
-    std::cout << "Ola mundo\n";
+int main()
+{
+int numero;
+
+scanf("%d",&numero);
+
+if (((numero % 5)==0) && ((numero % 3)==0)){
+printf("O NUMERO E DIVISIVEL\n");
+}
+else{
+printf("O NUMERO NAO E DIVISIVEL\n");
+}
+return 0;
 }
