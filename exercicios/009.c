9fbb76fc70bf3f4cbb6314c486067ef160680afb
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 15:05:08 -0300

    Update 009.c

--- exercicios/009.c
+++ exercicios/009.c
@@ -1,18 +1,42 @@
 /*
-Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre
-a temperatura em graus Celsius.
-C = (5 * (F-32) / 9).
+Desenvolver um algoritmo com as opções de calcular e imprimir o volume e a área da superfície de um cone reto, um cilindro ou uma esfera. O algoritmo deverá ler a 
+opção da figura desejada (cone / cilindro / esfera) e de acordo com a opção escolhida calcular o volume e a área da superfície da figura pedida. Fórmulas:
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#define pi 3.1415
+
 int main()
 {
-    float grausC, grausF;
-    printf("Digite a temperatura em graus Farenheit: ");
-    scanf("%f", &grausF);
-    grausC = (5 * (grausF - 32) / 9);
-    printf("%.2f graus Farenheit correspondem a %.2f graus Celsius", grausF, grausC);
-    return 0;
+
+   float raio, altura, volume, area;
+   char tipo[10];
+
+   scanf("%f",&raio);
+   scanf("%f",&altura);
+   scanf("%s",tipo);
+
+   if (strcmp(tipo,"CONE")==0){
+       volume = ((pi * pow(raio,2)) * altura) / 3;
+       area = (pi * raio) * sqrt(pow(raio,2) + pow(altura,2));
+       printf("VOLUME = %.2f\n",volume);
+       printf("AREA = %.2f\n",area);
+   }
+
+else if (strcmp(tipo,"CILINDRO")==0){
+       volume = ((pi * pow(raio,2)) * altura);
+       area = (2 * pi) * (raio * altura);
+       printf("VOLUME = %.2f\n",volume);
+       printf("AREA = %.2f\n",area);
+   }
+
+   else if (strcmp(tipo,"ESFERA")==0){
+       volume = (pi * 4 * pow(raio,3)) / 3;
+       area = (4 * (pi * pow(raio,2)));
+       printf("VOLUME = %.2f\n",volume);
+       printf("AREA = %.2f\n",area);
+   }
+
+return 0;
 }
